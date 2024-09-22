6e02bf5ea63c0504a919a093210b5b15802717e2
Ben Wolsieffer <benwolsieffer@gmail.com>
2024-09-22 19:54:12 -0400

    sd_card: support hotplugging

--- firmware/audio/app/src/sd_card.c
+++ firmware/audio/app/src/sd_card.c
@@ -1,24 +1,35 @@
 #include "sd_card.h"
 
 #include <ff.h>
+#include <zephyr/drivers/sdhc.h>
 #include <zephyr/fs/fs.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/storage/disk_access.h>
 
 LOG_MODULE_REGISTER(sd_card);
 
-static struct sd_card {
-    // Config
-    const char* name;
+static void sd_card_work_handler(struct k_work* work);
+static K_WORK_DELAYABLE_DEFINE(sd_card_work, sd_card_work_handler);
 
-    // Resources
+static const struct sd_card_config {
+    const char* name;
+    const struct device* sd;
+    struct k_work_delayable* work;
+} sd_card_config = {
+    .name = "SD",
+    .sd = DEVICE_DT_GET(DT_NODELABEL(sdhc0)),
+    .work = &sd_card_work,
+};
+
+static struct sd_card_data {
     struct fs_mount_t mount;
     FATFS fat_fs;
 
     // State
     bool init;
+    bool disk_init;
+    bool mounted;
 } sd_card = {
-    .name = "SD",
     .mount =
         {
             .type = FS_FATFS,
@@ -59,25 +70,97 @@ static int sd_card_print_info(const char* disk_pdrv) {
     return 0;
 }
 
+static int sd_card_inserted(void) {
+    const struct sd_card_config* config = &sd_card_config;
+    struct sd_card_data* data = &sd_card;
+    int ret;
+
+    LOG_INF("SD card inserted");
+
+    if (!data->disk_init) {
+        ret = disk_access_ioctl(config->name, DISK_IOCTL_CTRL_INIT, NULL);
+        if (ret < 0) {
+            LOG_ERR("failed to initialize SD card: (err %d)", ret);
+            return ret;
+        }
+
+        data->disk_init = true;
+
+        ret = sd_card_print_info(config->name);
+        if (ret < 0) return ret;
+    }
+
+    if (!data->mounted) {
+        ret = fs_mount(&data->mount);
+        if (ret != FR_OK) {
+            LOG_ERR("failed to mount SD card: (err %d)", ret);
+            return -ENODEV;
+        }
+
+        data->mounted = true;
+    }
+
+    return 0;
+}
+
+static int sd_card_removed(void) {
+    const struct sd_card_config* config = &sd_card_config;
+    struct sd_card_data* data = &sd_card;
+    int ret;
+
+    LOG_INF("SD card removed");
+
+    if (data->mounted) {
+        ret = fs_unmount(&data->mount);
+        if (ret != FR_OK) {
+            LOG_ERR("failed to unmount SD card: (err %d)", ret);
+            return -ENODEV;
+        }
+
+        data->mounted = false;
+    }
+
+    if (data->disk_init) {
+        ret = disk_access_ioctl(config->name, DISK_IOCTL_CTRL_DEINIT, NULL);
+        if (ret < 0) {
+            LOG_ERR("failed to deinitialize SD card: (err %d)", ret);
+            return ret;
+        }
+
+        data->disk_init = false;
+    }
+
+    return 0;
+}
+
+static void sd_card_work_handler(struct k_work* work) {
+    const struct sd_card_config* config = &sd_card_config;
+    if (sdhc_card_present(config->sd)) {
+        sd_card_inserted();
+    } else {
+        sd_card_removed();
+    }
+}
+
+static void sd_card_interrupt(const struct device* dev, int reason,
+                              const void* user_data) {
+    const struct sd_card_config* config = &sd_card_config;
+
+    // Wait 100ms to allow bouncing to settle
+    // TODO: probably shouldn't run this on the global workqueue
+    k_work_reschedule(config->work, K_MSEC(100));
+}
+
 int sd_card_init(void) {
-    struct sd_card* s = &sd_card;
-    if (s->init) return -EALREADY;
+    const struct sd_card_config* config = &sd_card_config;
+    struct sd_card_data* data = &sd_card;
+    int ret;
+    if (data->init) return -EALREADY;
 
-    int ret = disk_access_init(s->name);
-    if (ret < 0) {
-        LOG_ERR("failed to initialize SD card: (err %d)", ret);
-        return ret;
-    }
+    ret = sdhc_enable_interrupt(config->sd, sd_card_interrupt,
+                                SDHC_INT_INSERTED | SDHC_INT_REMOVED, NULL);
+    k_work_schedule(config->work, K_NO_WAIT);
 
-    ret = sd_card_print_info(s->name);
-    if (ret < 0) return ret;
-
-    ret = fs_mount(&s->mount);
-    if (ret != FR_OK) {
-        LOG_ERR("failed to mount SD card: (err %d)", ret);
-        return -ENODEV;
-    }
-
-    s->init = true;
+    data->init = true;
     return 0;
 }
